26da5635aad149ef951230c5952bdcc277be5ff7
Irwin <irgranados16@gmail.com>
2020-08-29 11:57:41 -0500

    add navbar logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ export const NavBar = () => {
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-          <a class="navbar-brand text-monospace font-weight-bold" href="">
-            I M
+          <a class="navbar-brand" href="">
+            <img src="https://docs.google.com/uc?id=1Gs9blxw2z_E1jdeW7b05LJrDar0eZ5JC" width="30" height="30" alt="IM Nav Logo" />
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,12 +14,12 @@ export const NavBar = () => {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav nav-fill w-100">
               <li class="nav-item active">
-                <a class="nav-link" href="{{ url_for('resume') }}">
+                <a class="nav-link disable" href="">
                   Resume
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link disable" href="{{ url_for('contact') }}" tabindex="-1">
+                <a class="nav-link disable" href="" tabindex="-1">
                   Contact
                 </a>
               </li>
