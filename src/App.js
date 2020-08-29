bd11b0949d836443e6f50c9971f5c6a2d72ce2b7
Irwin <irgranados16@gmail.com>
2020-08-29 13:11:58 -0500

    add home page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
+import { HomePage } from "./components/HomePage";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+export const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <div className="card border-0">
+            <img className="card-img-top" src="https://docs.google.com/uc?id=1XpU1FHq4qIZva_xaBYwFQ8tlhZVDP_Dx" alt="irwin headshot" />
+            <div className="card-body">
+              <h2 className="card-title"> Irwin Marcano</h2>
+              <hr />
+              <p className="card-text h4">Web Developer</p>
+              {/* Weather Information */}
+              <img src="https://openweathermap.org/img/wn/04d.png" alt="Weather icon" height="100" width="100" />
+              <p className="lead font-italic">Chicago | Broken Clouds</p>
+              <p className="h1">
+                {" "}
+                77 <span>&#176;</span> F{" "}
+                <span className="lead">
+                  {" "}
+                  25 <span>&#176;</span> C
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-8 offset-1">
+          <div className="jumbotron bg-transparent m-0">
+            <blockquote className="blockquote">
+              <p className="lead">
+                Certified Web Developer with skills in Front-End Development using HTML, CSS, JavaScript, and Vue. Experience with object-oriented programming using Python Flask and Ruby on Rails; Cloud Computing knowledge with deploying applications
+                to AWS and Version Control.
+              </p>
+              <a className="btn btn-dark" href="#resumeLink" role="button">
+                {" "}
+                Learn more
+              </a>
+            </blockquote>
+            <hr />
+          </div>
+
+          <h3 className="mb-5 text-center">Get in touch</h3>
+
+          <div className="row justify-content-around">
+            <a className="" href="https://github.com/irwinx16" target="_black">
+              <img src="https://docs.google.com/uc?id=1qenoe8B7YbccuDpLAmO312lxnVhy21s1" className="w-75" />
+            </a>
+            <a className="" href="https://www.linkedin.com/in/irwinx16/" target="_black">
+              <img src="https://docs.google.com/uc?id=1DKGdcSWxeKopkfW_jeppr8xhrPbQW5BO" className="w-75" />
+            </a>
+            <a className="" href="mailto:irgranados16@gmail.com?subject=Interview Request - <POSITION>" target="_black">
+              <img src="https://docs.google.com/uc?id=1XoFHduV6hfyasUBGMGYBNBQr6jXNnC_p" className="w-75" />
+            </a>
+            <a className="" href="https://drive.google.com/file/d/1DSBaBHLeaE5_-lyIRz9-EpdfBLmEF8PO/preview" target="_black">
+              <img src="https://docs.google.com/uc?id=11DqVBbRU5SkAaCGYHovOj6lNZ12HvfDx" className="w-75" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
