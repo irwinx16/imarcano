d625b6c1ac7d51117d2d1f91f3c9c74de028b0b7
Irwin <irgranados16@gmail.com>
2020-08-30 17:59:40 -0500

    replace class with className

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-          <Link to="/" class="navbar-brand">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
             <img src="https://docs.google.com/uc?id=1Gs9blxw2z_E1jdeW7b05LJrDar0eZ5JC" width="30" height="30" alt="IM Nav Logo" />
           </Link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav nav-fill w-100">
-              <li class="nav-item active">
-                <Link to="/resume" class="nav-link disable">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav nav-fill w-100">
+              <li className="nav-item active">
+                <Link to="/resume" className="nav-link disable">
                   Resume
                 </Link>
               </li>
-              <Link to="/contact" class="nav-item">
-                <a class="nav-link disable" href="" tabindex="-1">
+              <Link to="/contact" className="nav-item">
+                <a className="nav-link disable" href="" tabindex="-1">
                   Contact
                 </a>
               </Link>
