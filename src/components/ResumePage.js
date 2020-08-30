eaea96a27d8a62c3c64e9518cd1bfc7c7cd427b0
Irwin <irgranados16@gmail.com>
2020-08-30 18:56:36 -0500

    update resumepage with current information

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -10,7 +10,7 @@ export const ResumePage = () => {
           <h3 className="lead">Web Developer</h3>
         </div>
       </div>
-      <div className="row mt-3">
+      <div className="row mt-5">
         <div className="col-sm-12 col-md-4">
           <h3>
             Skills
@@ -18,13 +18,13 @@ export const ResumePage = () => {
           </h3>
           <p>
             <ul className="list-unstyled">
-              <li>Python, Django, Ruby,</li>
-              <li>Rails, JavaScript, VueJs,</li>
+              <li>Python, Flask, Ruby,</li>
+              <li>Rails, JavaScript, React,</li>
               <li>NodeJs, ExpressJs, AWS,</li>
               <li>PostgreSQL, Heroku, Git,</li>
               <li>HTML, Bootstrap, CSS,</li>
-              <li>Front End/Back End</li>
-              <li>Bilingual (Spanish)</li>
+              <li>Problem-Solving, Research</li>
+              <li>Fluent in Spanish</li>
             </ul>
           </p>
           <h3 className="mt-5">
@@ -33,17 +33,20 @@ export const ResumePage = () => {
           </h3>
           <p>
             <span className="font-weight-bold">General Assembly</span> <br />
-            <span>Web Development Immersive Certificate</span> <br />
+            <span>Web Development Immersive</span> <br />
+            <span className="font-italic">Certificate</span> <br />
             <span className="font-weight-lighter small">Mar 2018 - Jun 2018</span>
           </p>
           <p>
             <span className="font-weight-bold">Harry S. Truman College</span> <br />
             <span>Adult Education Coursework</span> <br />
+            <span className="font-italic">Vocational</span> <br />
             <span className="font-weight-lighter small">Nov 2016 - Dec 2017</span>
           </p>
           <p>
             <span className="font-weight-bold">Rodolfo Loero Arismendi Institute of Technology</span> <br />
-            <span>Human Resources Management Associates Degree</span> <br />
+            <span>Human Resources Management</span> <br />
+            <span className="font-italic">Associates Degree</span> <br />
             <span className="font-weight-lighter small">Sep 2010 - May 2014</span>
           </p>
         </div>
@@ -53,57 +56,78 @@ export const ResumePage = () => {
             <hr />
           </h3>
           <p>
-            Technology enthusiast with over seven years of professional experience. I transitioned into Web Development two years ago. My experience has focused on: web development tools and best practices, Front-End and Back-End technologies,
-            object-oriented programming, problem-solving and researching. I’d love to explore new ways technology can be used to improve the world and collaborate with others to make it happen.
+            Certified Web Developer with skills in Front-End Development using HTML, CSS, JavaScript, and React. Experience with object-oriented programming using Python Flask and Ruby on Rails; Cloud Computing knowledge with deploying applications
+            to AWS and Version Control. I’d love to explore new ways technology can be used to improve the world and collaborate with others to make it happen.
           </p>
           <h3 className="mt-5">
             Experience
             <hr />
           </h3>
-          <p>
-            <span className="font-weight-bold">Freelance Web Development,​</span> <span className="font-italic">​Web Developer</span> <br />
-            <span className="font-weight-lighter small">Feb 2019 - Present | Chicago, IL.</span>
-          </p>
-          <p>
-            <ul>
-              <li>Developed websites in Squarespace; Implemented additional features using Javascript, jQuery, and CSS according to client needs</li>
-              <li>Developed mobile responsive websites using HTML, CSS, Bootstrap, and Javascript. Knowledge of VueJs for Web Apps, deployment to Github, and use of Gulp to make development easier</li>
-            </ul>
-          </p>
-          <p className="mt-4">
-            <span className="font-weight-bold">General Assembly,​ </span> <span className="font-italic">​Web Development Course</span> <br />
-            <span className="font-weight-lighter small">March 2018 - June 2018 | Chicago, IL.</span>
-          </p>
-          <p>
-            <span className="font-weight-bold">Parking App | Check for Parking Restrictions</span>
-          </p>
-          <p>
-            <ul>
-              <li>Made AJAX call to Chicago API and linked JSON to Google Maps</li>
-              <li>Used Git to collaborate with team members; working on branches and resolving merging issues</li>
-              <li>Created logic and styled frontend ejs templates</li>
-              <li>Deployed application using Heroku</li>
-            </ul>
-          </p>
-          <p className="mb-3">
-            <span className="font-weight-bold">Technologies used:</span> <br />
-            NodeJS, ExpressJS, MongoDB, JavaScript, HTML, Bootstrap, CSS
-          </p>
-          <p>
-            <span className="font-weight-bold">EMS | Schedule and Manage your employees</span>
-          </p>
-          <p>
-            <ul>
-              <li>Build the backend API controllers and models</li>
-              <li>Collaborated in creating and styling React frontend</li>
-              <li>Used Git to pull the latest changes, push and merge branches</li>
-              <li>Deployed application using Heroku</li>
-            </ul>
-          </p>
-          <p className="mb-5">
-            <span className="font-weight-bold">Technologies used:</span> <br />
-            Ruby, Sinatra, PostgreSQL, JavaScript, React, HTML, Bootstrap, Git
-          </p>
+          <div id="ta_nu" className="mb-4">
+            <p>
+              <span className="font-weight-bold">Trilogy Education, ​</span>
+              <span className="font-italic">​Teaching Assistant - Web Development Bootcamp</span> <br />
+              <span className="font-weight-lighter small">Jun 2020 - Present | Chicago, IL.</span>
+            </p>
+            <p>
+              <ul>
+                <li>Assist students with activities and projects regarding front-end and back-end technologies</li>
+              </ul>
+            </p>
+          </div>
+
+          <div id="freelancer" className="mb-4">
+            <p>
+              <span className="font-weight-bold">Freelance Web Development, ​</span>
+              <span className="font-italic">​Web Developer</span> <br />
+              <span className="font-weight-lighter small">Feb 2019 - Present | Chicago, IL.</span>
+            </p>
+            <p>
+              <ul>
+                <li>Developed full static websites in Squarespace; Implemented additional features using jQuery, and CSS according to client needs</li>
+                <li>Built mobile responsive website using HTML, CSS, Bootstrap, and Javascript. Knowledge of VueJs for Web Apps, deployment to Github, and use of Gulp for faster development</li>
+                <li>Development and maintenance of CMS, Websites and Web Applications using front-end technologies</li>
+              </ul>
+            </p>
+          </div>
+
+          <div id="general_assembly" className="mb-4">
+            <p>
+              <span className="font-weight-bold">General Assembly,​ </span>
+              <span className="font-italic">​Web Development Course</span> <br />
+              <span className="font-weight-lighter small">March 2018 - June 2018 | Chicago, IL.</span>
+            </p>
+            <p>
+              <span className="font-weight-bold">Parking App ​|​ Check for Parking Restrictions</span>
+            </p>
+            <p>
+              <ul>
+                <li>Coded AJAX call to Chicago API and linked response to Google Maps</li>
+                <li>Wrote code to test JSON responses were correct</li>
+                <li>Used Version Control technologies for collaboration</li>
+                <li>Created and styled forms and logic for frontend templates</li>
+              </ul>
+            </p>
+            <p>
+              <span className="font-weight-bold">Technologies used:</span> <br />
+              NodeJS, ExpressJS, MongoDB, JavaScript, HTML, Bootstrap, CSS
+            </p>
+            <p>
+              <span className="font-weight-bold">EMS | Schedule and Manage your employees</span>
+            </p>
+            <p>
+              <ul>
+                <li>Built Backend RESTfull API controllers and models</li>
+                <li>Collaborated in creating and styling React frontend</li>
+                <li>Used Version Control to keep the project up to date</li>
+                <li>Created SQL database to store and retrieve data</li>
+              </ul>
+            </p>
+            <p>
+              <span className="font-weight-bold">Technologies used:</span> <br />
+              Ruby, Sinatra, PostgreSQL, JavaScript, React, HTML, Bootstrap, Git
+            </p>
+          </div>
         </div>
       </div>
     </div>
