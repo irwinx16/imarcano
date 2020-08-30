4e1e771d6c9f6db6c5995cc33dd71fbc8dd0f184
Irwin <irgranados16@gmail.com>
2020-08-30 17:49:20 -0500

    add react-router and configure routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
+import { ResumePage } from "./components/ResumePage";
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
-      <HomePage />
-    </div>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/resume" component={ResumePage} />
+    </Router>
   );
 }
 
---
+++ src/components/ResumePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ResumePage = () => {
+  return (
+    <div>
+      <h1>Resume Page</h1>
+      <p>Said something</p>
+    </div>
+  );
+};
