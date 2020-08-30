488f6e3b8467e048edfed86be680871d113157e8
Irwin <irgranados16@gmail.com>
2020-08-30 17:56:48 -0500

    set up navbar to work with link and react router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-          <a class="navbar-brand" href="">
+          <Link to="/" class="navbar-brand">
             <img src="https://docs.google.com/uc?id=1Gs9blxw2z_E1jdeW7b05LJrDar0eZ5JC" width="30" height="30" alt="IM Nav Logo" />
-          </a>
+          </Link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav nav-fill w-100">
               <li class="nav-item active">
-                <a class="nav-link disable" href="">
+                <Link to="/resume" class="nav-link disable">
                   Resume
-                </a>
+                </Link>
               </li>
-              <li class="nav-item">
+              <Link to="/contact" class="nav-item">
                 <a class="nav-link disable" href="" tabindex="-1">
                   Contact
                 </a>
-              </li>
+              </Link>
             </ul>
           </div>
         </div>
