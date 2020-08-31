6b37c5ca46133a56c0becd4d3bc6d9951a8372fb
Irwin <irgranados16@gmail.com>
2020-08-30 19:01:54 -0500

    clean up code

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -10,6 +10,7 @@ export const ResumePage = () => {
           <h3 className="lead">Web Developer</h3>
         </div>
       </div>
+
       <div className="row mt-5">
         <div className="col-sm-12 col-md-4">
           <h3>
@@ -27,6 +28,7 @@ export const ResumePage = () => {
               <li>Fluent in Spanish</li>
             </ul>
           </p>
+
           <h3 className="mt-5">
             Education
             <hr />
@@ -50,6 +52,7 @@ export const ResumePage = () => {
             <span className="font-weight-lighter small">Sep 2010 - May 2014</span>
           </p>
         </div>
+
         <div className="col-sm-12 col-md-8">
           <h3>
             Summary
@@ -59,6 +62,7 @@ export const ResumePage = () => {
             Certified Web Developer with skills in Front-End Development using HTML, CSS, JavaScript, and React. Experience with object-oriented programming using Python Flask and Ruby on Rails; Cloud Computing knowledge with deploying applications
             to AWS and Version Control. I’d love to explore new ways technology can be used to improve the world and collaborate with others to make it happen.
           </p>
+
           <h3 className="mt-5">
             Experience
             <hr />
@@ -69,11 +73,9 @@ export const ResumePage = () => {
               <span className="font-italic">​Teaching Assistant - Web Development Bootcamp</span> <br />
               <span className="font-weight-lighter small">Jun 2020 - Present | Chicago, IL.</span>
             </p>
-            <p>
-              <ul>
-                <li>Assist students with activities and projects regarding front-end and back-end technologies</li>
-              </ul>
-            </p>
+            <ul>
+              <li>Assist students with activities and projects regarding front-end and back-end technologies</li>
+            </ul>
           </div>
 
           <div id="freelancer" className="mb-4">
@@ -82,13 +84,11 @@ export const ResumePage = () => {
               <span className="font-italic">​Web Developer</span> <br />
               <span className="font-weight-lighter small">Feb 2019 - Present | Chicago, IL.</span>
             </p>
-            <p>
-              <ul>
-                <li>Developed full static websites in Squarespace; Implemented additional features using jQuery, and CSS according to client needs</li>
-                <li>Built mobile responsive website using HTML, CSS, Bootstrap, and Javascript. Knowledge of VueJs for Web Apps, deployment to Github, and use of Gulp for faster development</li>
-                <li>Development and maintenance of CMS, Websites and Web Applications using front-end technologies</li>
-              </ul>
-            </p>
+            <ul>
+              <li>Developed full static websites in Squarespace; Implemented additional features using jQuery, and CSS according to client needs</li>
+              <li>Built mobile responsive website using HTML, CSS, Bootstrap, and Javascript. Knowledge of VueJs for Web Apps, deployment to Github, and use of Gulp for faster development</li>
+              <li>Development and maintenance of CMS, Websites and Web Applications using front-end technologies</li>
+            </ul>
           </div>
 
           <div id="general_assembly" className="mb-4">
@@ -100,29 +100,26 @@ export const ResumePage = () => {
             <p>
               <span className="font-weight-bold">Parking App ​|​ Check for Parking Restrictions</span>
             </p>
-            <p>
-              <ul>
-                <li>Coded AJAX call to Chicago API and linked response to Google Maps</li>
-                <li>Wrote code to test JSON responses were correct</li>
-                <li>Used Version Control technologies for collaboration</li>
-                <li>Created and styled forms and logic for frontend templates</li>
-              </ul>
-            </p>
+            <ul>
+              <li>Coded AJAX call to Chicago API and linked response to Google Maps</li>
+              <li>Wrote code to test JSON responses were correct</li>
+              <li>Used Version Control technologies for collaboration</li>
+              <li>Created and styled forms and logic for frontend templates</li>
+            </ul>
             <p>
               <span className="font-weight-bold">Technologies used:</span> <br />
               NodeJS, ExpressJS, MongoDB, JavaScript, HTML, Bootstrap, CSS
             </p>
+
             <p>
               <span className="font-weight-bold">EMS | Schedule and Manage your employees</span>
             </p>
-            <p>
-              <ul>
-                <li>Built Backend RESTfull API controllers and models</li>
-                <li>Collaborated in creating and styling React frontend</li>
-                <li>Used Version Control to keep the project up to date</li>
-                <li>Created SQL database to store and retrieve data</li>
-              </ul>
-            </p>
+            <ul>
+              <li>Built Backend RESTfull API controllers and models</li>
+              <li>Collaborated in creating and styling React frontend</li>
+              <li>Used Version Control to keep the project up to date</li>
+              <li>Created SQL database to store and retrieve data</li>
+            </ul>
             <p>
               <span className="font-weight-bold">Technologies used:</span> <br />
               Ruby, Sinatra, PostgreSQL, JavaScript, React, HTML, Bootstrap, Git
