2a8aafa4dee81dfc5bced8482df73f653cb5f64f
Irwin <irgranados16@gmail.com>
2021-01-30 13:15:20 -0600

    clean up html

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -56,6 +56,7 @@ export const HomePage = () => {
             </div>
           </div>
         </div>
+		
         <div className="col-8 offset-1">
           <div className="jumbotron bg-transparent m-0 p-1">
             <blockquote className="blockquote">
@@ -63,27 +64,25 @@ export const HomePage = () => {
                 Self-motivated Developer experienced with a wide range of programming utilities and languages. Knowledgeable of backend and frontend development requirements. Collaborative problem-solver with an analytical mindset and excellent
                 technical abilities.
               </p>
-              <Link to="/resume" className="btn btn-dark" role="button">
-                Learn more
-              </Link>
+              <Link to="/resume" className="btn btn-dark" role="button">Learn more</Link>
             </blockquote>
             <hr />
           </div>
 
-          <h2 className="mb-5 text-center">Get in touch</h2>
 
+          <h2 className="mb-5 text-center">Get in touch</h2>
           <div className="row justify-content-around">
             <a href="https://github.com/irwinx16" target="_black">
-              <img src="https://i.imgur.com/0Vbe5RF.png" className="w-75" alt="github_icon" />
+              <img src="https://i.imgur.com/0Vbe5RF.png" alt="github_icon" />
             </a>
             <a href="https://www.linkedin.com/in/irwinx16/" target="_black">
-              <img src="https://i.imgur.com/n9xhCX4.png" className="w-75" alt="linkedin_icon" />
+              <img src="https://i.imgur.com/n9xhCX4.png" alt="linkedin_icon" />
             </a>
             <a href="mailto:irgranados16@gmail.com?subject=Interview Request - <POSITION>" target="_black">
-              <img src="https://i.imgur.com/TYE6LWC.png" className="w-75" alt="email_icon" />
+              <img src="https://i.imgur.com/TYE6LWC.png" alt="email_icon" />
             </a>
             <a href="https://drive.google.com/file/d/1dyVZ1NQLvEVGhtl7pDSJVvrahJo1GGUN/preview" target="_black">
-              <img src="https://i.imgur.com/zPMpf6Y.png" className="w-75" alt="resume_icon" />
+              <img src="https://i.imgur.com/zPMpf6Y.png" alt="resume_icon" />
             </a>
           </div>
         </div>
