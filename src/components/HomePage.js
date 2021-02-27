c598bbafa26f85619046f1b2214854a73f4071ca
Irwin <irgranados16@gmail.com>
2021-02-27 15:22:09 -0600

    apply css reset and organize bootstrap div layout for homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,49 +3,60 @@ import { Link } from "react-router-dom";
 import { Weather } from "./Weather";
 
 export const HomePage = () => {
-  
   return (
     <div className="container">
-      <div className="row mt-5">
-        <div className="col-lg-3">
+      <div className="row mt-5 align-items-center" id="personalInfo">
+        <div className="col-md-3">
           <div className="card border-0">
-            <img className="card-img-top" src="https://i.imgur.com/6nWHsPp.jpg" alt="irwin-headshot" />
-            <div className="card-body">
-              <h2 className="card-title"> Irwin Marcano</h2>
+            <img src="https://i.imgur.com/6nWHsPp.jpg" className="card-img-top" alt="headshotpicture" />
+            <div className="card-body px-0">
+              <h2 className="card-title">Irwin Marcano</h2>
+              <h5 className="card-text">Web Developer</h5>
               <hr />
-              <p className="card-text h4">Web Developer</p>
-			  <Weather />
             </div>
           </div>
         </div>
-		
-        <div className="col-8 offset-1">
-          <div className="jumbotron bg-transparent m-0 p-1">
-            <blockquote className="blockquote">
-              <p className="lead">
-                Self-motivated Developer experienced with a wide range of programming utilities and languages. Knowledgeable of backend and frontend development requirements. Collaborative problem-solver with an analytical mindset and excellent
-                technical abilities.
-              </p>
-              <Link to="/resume" className="btn btn-dark" role="button">Learn more</Link>
-            </blockquote>
-            <hr />
-          </div>
 
+        <div className="col-md-8 offset-md-1">
+          <p className="lead">Self-motivated Developer experienced with a wide range of programming utilities and languages. Knowledgeable of backend and frontend development requirements. Collaborative problem-solver with an analytical mindset and excellent technical abilities.</p>
 
-          <h2 className="mb-5 text-center">Get in touch</h2>
+          <Link to="/resume" className="btn btn-dark" role="button">
+            Learn more
+          </Link>
+          <hr />
+        </div>
+      </div>
+
+      <div className="row" id="contactInfo">
+        <div className="col-md-3 mb-3 text-center text-md-left order-last order-md-1">
+          <Weather />
+        </div>
+
+        <div className="col-md-8 offset-md-1 order-first order-md-2" id="contactIcons">
+          <h2 className="text-center mb-3">Get in touch</h2>
           <div className="row justify-content-around">
-            <a href="https://github.com/irwinx16" target="_black">
-              <img src="https://i.imgur.com/0Vbe5RF.png" alt="github_icon" />
-            </a>
-            <a href="https://www.linkedin.com/in/irwinx16/" target="_black">
-              <img src="https://i.imgur.com/n9xhCX4.png" alt="linkedin_icon" />
-            </a>
-            <a href="mailto:irgranados16@gmail.com?subject=Interview Request - <POSITION>" target="_black">
-              <img src="https://i.imgur.com/TYE6LWC.png" alt="email_icon" />
-            </a>
-            <a href="https://drive.google.com/file/d/1dyVZ1NQLvEVGhtl7pDSJVvrahJo1GGUN/preview" target="_black">
-              <img src="https://i.imgur.com/zPMpf6Y.png" alt="resume_icon" />
-            </a>
+            <div className="mb-3">
+              <a href="https://github.com/irwinx16" target="_black">
+                <img src="https://i.imgur.com/0Vbe5RF.png" alt="github_icon" />
+              </a>
+            </div>
+            <div className="mb-3">
+              <a href="https://www.linkedin.com/in/irwinx16/" target="_black">
+                <img src="https://i.imgur.com/n9xhCX4.png" alt="linkedin_icon" />
+              </a>
+            </div>
+
+            <div className="mb-3">
+              <a href="mailto:irgranados16@gmail.com?subject=Interview Request - <POSITION>" target="_black">
+                <img src="https://i.imgur.com/TYE6LWC.png" alt="email_icon" />
+              </a>
+            </div>
+
+            <div className="mb-3">
+              <a href="https://drive.google.com/file/d/1dyVZ1NQLvEVGhtl7pDSJVvrahJo1GGUN/preview" target="_black">
+                <img src="https://i.imgur.com/zPMpf6Y.png" alt="resume_icon" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
