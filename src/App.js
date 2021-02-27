bef2e6d2203c18510261f9dc2407636cd458a379
Irwin <irgranados16@gmail.com>
2021-02-27 15:29:04 -0600

    remove css reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { HomePage } from "./components/HomePage";
