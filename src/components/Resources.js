023813afad5c83279b8b7497b213c57e3fa55b9f
Irwin <irgranados16@gmail.com>
2021-07-31 14:07:28 -0500

    adds third resource card

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import zoomGuideFile from "../markdowns/zoomGuide.md";
 import slackGuideFile from "../markdowns/slackGuide.md";
+import githubGuideFile from "../markdowns/githubGuide.md";
 
 export const Resources = () => {
   const [zoomGuide, setZoomGuide] = useState("");
   const [slackGuide, setSlackGuide] = useState("");
+  const [gihubGuide, setGithubGuide] = useState("");
 
   useEffect(() => {
     fetch(zoomGuideFile)
@@ -15,6 +17,10 @@ export const Resources = () => {
     fetch(slackGuideFile)
       .then((res) => res.text())
       .then((text) => setSlackGuide(text));
+
+    fetch(githubGuideFile)
+      .then((res) => res.text())
+      .then((text) => setGithubGuide(text));
   }, []);
   return (
     <div className="container">
@@ -55,22 +61,20 @@ export const Resources = () => {
             </div>
 
             {/* --------Card Block---------- */}
-            {/* <div className="card">
+            <div className="card">
               <div className="card-header" id="headingThree">
                 <h5 className="mb-0">
                   <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
+                    GitHub Guide
                   </button>
                 </h5>
               </div>
               <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                 <div className="card-body">
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
-                  coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of
-                  them accusamus labore sustainable VHS.
+                  <ReactMarkdown children={gihubGuide} />
                 </div>
               </div>
-            </div> */}
+            </div>
             {/* --------Card Block---------- */}
           </div>
         </div>
